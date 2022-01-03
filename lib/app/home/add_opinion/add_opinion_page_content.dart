2417efc0acca33aa7e3477eae0f96d00e3ac37aa
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2022-01-03 14:12:33 +0100

    Implement adding levels

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var basicName = '';
+  var mediumName = '';
+  var hardName = '';
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Podobało się? Podziel się opinią i oceń apkę!'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+              decoration: const InputDecoration(
+                  hintText: 'Czy trudno było być Stajennym?'),
+              onChanged: (newValue) {
+                setState(() {
+                  basicName = newValue;
+                });
+              }),
+          TextField(
+              decoration: const InputDecoration(
+                  hintText: 'Jak podobał Ci się poziom trudności Giermka?'),
+              onChanged: (newValue) {
+                setState(() {
+                  mediumName = newValue;
+                });
+              }),
+          TextField(
+              decoration: const InputDecoration(
+                  hintText: 'Co sądzisz o poziomie trudności Rycerza?'),
+              onChanged: (newValue) {
+                setState(() {
+                  hardName = newValue;
+                });
+              }),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('levels').add({
+                'basic': basicName,
+                'medium': mediumName,
+                'hard': hardName,
+                'opinion': 3.0,
+              });
+            },
+            child: const Text('Dodaj opinię'),
+          ),
+        ],
+      ),
     );
   }
 }
