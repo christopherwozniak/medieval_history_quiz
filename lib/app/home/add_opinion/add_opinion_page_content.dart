780fbd23846f5e8d5cda8d04eb5fe113169d4d53
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2022-01-05 14:04:39 +0100

    Change bottom navigation bar index to 0 on save

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -2,9 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddOpinionPageContent extends StatefulWidget {
-  const AddOpinionPageContent({
-    Key? key,
-  }) : super(key: key);
+  const AddOpinionPageContent({Key? key, required this.onSave})
+      : super(key: key);
+  final Function onSave;
 
   @override
   State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
@@ -18,57 +18,64 @@ class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-              decoration: const InputDecoration(
-                  hintText: 'Czy trudno było być Stajennym?'),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+                decoration: const InputDecoration(
+                    hintText: 'Czy trudno było być Stajennym?'),
+                onChanged: (newValue) {
+                  setState(() {
+                    basicName = newValue;
+                  });
+                }),
+            TextField(
+                decoration: const InputDecoration(
+                    hintText: 'Jak podobał Ci się poziom trudności Giermka?'),
+                onChanged: (newValue) {
+                  setState(() {
+                    mediumName = newValue;
+                  });
+                }),
+            TextField(
+                decoration: const InputDecoration(
+                    hintText: 'Co sądzisz o poziomie trudności Rycerza?'),
+                onChanged: (newValue) {
+                  setState(() {
+                    hardName = newValue;
+                  });
+                }),
+            Slider(
               onChanged: (newValue) {
                 setState(() {
-                  basicName = newValue;
+                  opinion = newValue;
                 });
-              }),
-          TextField(
-              decoration: const InputDecoration(
-                  hintText: 'Jak podobał Ci się poziom trudności Giermka?'),
-              onChanged: (newValue) {
-                setState(() {
-                  mediumName = newValue;
-                });
-              }),
-          TextField(
-              decoration: const InputDecoration(
-                  hintText: 'Co sądzisz o poziomie trudności Rycerza?'),
-              onChanged: (newValue) {
-                setState(() {
-                  hardName = newValue;
-                });
-              }),
-          Slider(
-            onChanged: (newValue) {
-              setState(() {
-                opinion = newValue;
-              });
-            },
-            value: opinion,
-            min: 1.0,
-            max: 6.0,
-            divisions: 10,
-            label: opinion.toString(),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              FirebaseFirestore.instance.collection('levels').add({
-                'basic': basicName,
-                'medium': mediumName,
-                'hard': hardName,
-                'opinion': opinion,
-              });
-            },
-            child: const Text('Dodaj opinię'),
-          ),
-        ],
+              },
+              value: opinion,
+              min: 1.0,
+              max: 6.0,
+              divisions: 10,
+              label: opinion.toString(),
+            ),
+            ElevatedButton(
+              onPressed:
+                  basicName.isEmpty || mediumName.isEmpty || hardName.isEmpty
+                      ? null
+                      : () {
+                          FirebaseFirestore.instance.collection('levels').add({
+                            'basic': basicName,
+                            'medium': mediumName,
+                            'hard': hardName,
+                            'opinion': opinion,
+                          });
+                          widget.onSave();
+                        },
+              child: const Text('Dodaj opinię'),
+            ),
+          ],
+        ),
       ),
     );
   }
