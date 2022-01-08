2e171ae8b2c0bd68efad35e409f528bb714136c8
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2022-01-08 20:51:04 +0100

    Move MyApp to app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:medieval_history_quiz/app/features/home/home_page.dart';
-import 'package:medieval_history_quiz/app/features/login/login_page.dart';
+
+import 'app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const FirstPageQuiz());
-  }
-}
-
-class FirstPageQuiz extends StatelessWidget {
-  const FirstPageQuiz({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          final user = snapshot.data;
-          if (user == null) {
-            return LoginPage();
-          }
-          return HomePage(user: user);
-        });
-  }
 }
