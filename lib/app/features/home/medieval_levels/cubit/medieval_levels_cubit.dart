a7dc01646af4aea9fcda78b3b9ecc064b7943908
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2022-05-22 17:28:58 +0200

    Delete unused files and update kotlin gradle

--- lib/app/features/home/medieval_levels/cubit/medieval_levels_cubit.dart
+++ lib/app/features/home/medieval_levels/cubit/medieval_levels_cubit.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
 
 part 'medieval_levels_state.dart';
--- lib/app/features/home/my_account/my_account_page_content.dart
+++ lib/app/features/home/my_account/my_account_page_content.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:medieval_history_quiz/app/cubit/firstpage_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
